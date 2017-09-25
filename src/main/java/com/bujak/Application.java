1d45f952552ad18e92b444dcd2e0b641afb2f617
Tomek Bujakowski <bujakowski@gmail.com>
2017-09-25 21:39:42 +0200

    Add ApplicationContext in main Application method

--- src/main/java/com/bujak/Application.java
+++ src/main/java/com/bujak/Application.java
@@ -3,6 +3,8 @@ package com.bujak;
 import com.bujak.model.Customer;
 import com.bujak.service.CustomerService;
 import com.bujak.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by bujak on 23.09.2017.
@@ -11,7 +13,9 @@ public class Application {
 
     public static void main(String[] args) {
 
-        CustomerService service = new CustomerServiceImpl();
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        CustomerService service = applicationContext.getBean("customerService", CustomerService.class);
 
         System.out.println(service.findAll().get(0).getFirstName());
     }
