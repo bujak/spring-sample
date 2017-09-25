dc8055ac665cec96216159314e6c7683b6deac9e
Tomek Bujakowski <bujakowski@gmail.com>
2017-09-25 22:20:02 +0200

    Declare beans in AppConfig

--- src/main/java/com/bujak/AppConfig.java
+++ src/main/java/com/bujak/AppConfig.java
@@ -1,5 +1,8 @@
 package com.bujak;
 
+import com.bujak.model.Customer;
+import com.bujak.repository.CustomerRepository;
+import com.bujak.repository.CustomerRepositoryImpl;
 import com.bujak.service.CustomerService;
 import com.bujak.service.CustomerServiceImpl;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +13,13 @@ public class AppConfig {
 
     @Bean(name = "customerService")
     public CustomerService getCustomerServie() {
-        return new CustomerServiceImpl();
+        CustomerServiceImpl service = new CustomerServiceImpl();
+        service.setCustomerRepository(getCustomerRepository());
+        return service;
+    }
+
+    @Bean(name = "customerRepository")
+    public CustomerRepository getCustomerRepository() {
+        return new CustomerRepositoryImpl();
     }
 }
