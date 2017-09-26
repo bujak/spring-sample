d1ddf3062366c92cb4bc211059bda7fe079c1709
Tomek Bujakowski <bujakowski@gmail.com>
2017-09-26 13:01:29 +0200

    Add singleton scope in CustomerService

--- src/main/java/com/bujak/service/CustomerServiceImpl.java
+++ src/main/java/com/bujak/service/CustomerServiceImpl.java
@@ -3,11 +3,14 @@ package com.bujak.service;
 import com.bujak.model.Customer;
 import com.bujak.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service("customerService")
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class CustomerServiceImpl implements CustomerService {
 
     private CustomerRepository customerRepository;
