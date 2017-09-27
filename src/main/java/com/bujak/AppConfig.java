4d2289f8944af9dcc1b29bc4edb885b33c2bad55
Tomek Bujakowski <bujakowski@gmail.com>
2017-09-27 12:31:14 +0200

    Add property source in AppConfig

--- src/main/java/com/bujak/AppConfig.java
+++ src/main/java/com/bujak/AppConfig.java
@@ -1,10 +1,19 @@
 package com.bujak;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan("com.bujak")
+@PropertySource("app.properties")
 public class AppConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
 }
